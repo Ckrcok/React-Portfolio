0af2b716b6cfdb526edaa21c62fc0f4a4794ed18
Dor <dorzairi@ymail.com>
2021-04-14 16:45:24 -0400

    More style and more componnets

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
 // import logo from "./logo.svg";
 import "./App.css";
+import Hey from "./components/Hey/Hey.jsx";
+import Image from "./assets/photo-1526925539332-aa3b66e35444.png";
+import Image2 from "./assets/scott-webb-VL-OvBppmzI-unsplash.jpg";
 
 function App() {
   return (
     <div className="App">
-      <img
-        src="./assets/photo-1526925539332-aa3b66e35444.png"
-        alt="laptop image"
-      ></img>
-      <h1 id="heythere">HEY THERE, I'M</h1>
-      <h1 id="dor">DOR</h1>
-      <h2 id="fullstack">FULLSTACK DEVELOPER</h2>
-      <p id="project">
-        I'm project addicted and inspiring video game developer
-      </p>
+      <div id="bg1">
+        <hr id="rec" />
+        <Hey />
+
+        <hr id="line1" />
+      </div>
+      <div id="bg2">NOICE</div>
     </div>
   );
 }
-
 export default App;
--- src/components/Hey/Hey.jsx
+++ src/components/Hey/Hey.jsx
@@ -1,4 +1,22 @@
 import React from "react";
 
-<h1 id="heythere">HEY THERE, I'M</h1>
-<h1 id="dor">DOR</h1>
+function Hey() {
+  return (
+    <div>
+      <h1 id="heythere" class="title">
+        HEY THERE, I'M
+      </h1>
+      <h1 id="dor" class="title">
+        DOR
+      </h1>
+      <h2 id="fullstack" class="title">
+        FULLSTACK DEVELOPER
+      </h2>
+      <p id="project" class="subtitle">
+        I'm project addicted and inspiring video game developer
+      </p>
+    </div>
+  );
+}
+
+export default Hey;
