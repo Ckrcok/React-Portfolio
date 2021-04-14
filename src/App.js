ecca330f8d3f26df28140c952dffbb9ede585799
Dor <dorzairi@ymail.com>
2021-04-14 11:11:17 -0400

    Some Styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src="./assets/photo-1526925539332-aa3b66e35444.png"
+        alt="laptop image"
+      ></img>
+      <h1 id="heythere">HEY THERE, I'M</h1>
+      <h1 id="dor">DOR</h1>
+      <h2 id="fullstack">FULLSTACK DEVELOPER</h2>
+      <p id="project">
+        I'm project addicted and inspiring video game developer
+      </p>
     </div>
   );
 }
---
+++ src/components/Hey/Hey.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+<h1 id="heythere">HEY THERE, I'M</h1>
+<h1 id="dor">DOR</h1>
