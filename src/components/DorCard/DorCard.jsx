ffe790f7ed0b20c2477f754f0bd1cc5f3bdae300
Dor <dorzairi@ymail.com>
2021-04-19 22:10:08 -0400

    Done project page

--- src/components/DorCard/DorCard.jsx
+++ src/components/DorCard/DorCard.jsx
@@ -5,7 +5,10 @@ function DorCard() {
   return (
     <span id="dorCard" className="card">
       <div className="title" id="card-id">
-        <img src="https://avatars.githubusercontent.com/u/56084381?v=4" />
+        <img
+          id="Avatar"
+          src="https://avatars.githubusercontent.com/u/56084381?v=4"
+        />
         <h5 id="name">Dor Zairi</h5>
       </div>
       <p className="subtitle is-5">Toronto, Canada</p>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,7 +4,50 @@ import "./Projects.css";
 function Projects() {
   return (
     <div>
-      <h1>My projects</h1>
+      <div>
+        <div id="project1Card" className="card">
+          <h1 className="title is-4" id="title1">
+            Connect-Four In Space
+          </h1>
+          <hr />
+          <p className="subtitle is-6" id="Descrip1">
+            My First project made by JS HTML And CSS
+          </p>
+        </div>
+        <img
+          id="Project1"
+          src="https://i.imgur.com/tPhb4Pw.png"
+          alt="Connect-Four In Space"
+        />
+      </div>
+      <div>
+        <div id="project2Card" className="card">
+          <h1 className="title is-4" id="title2">
+            Jean Model Website
+          </h1>
+          <hr />
+          <p className="subtitle is-6" id="Descrip2">
+            A website dedicted to my pet bunny and his model ablities, build
+            with express.js & MongoDB
+          </p>
+        </div>
+        <img
+          id="Project2"
+          src="https://i.imgur.com/QlGJoP3.png"
+          alt="Jean Model Website"
+        />
+        <div id="project3Card" className="card">
+          <h1 className="title is-4" id="title3">
+            Jean Model Website
+          </h1>
+          <hr />
+          <p className="subtitle is-6" id="Descrip3">
+            A website dedicted to my pet bunny and his model ablities, build
+            with express.js & MongoDB
+          </p>
+        </div>
+        <img id="Project3" src="https://i.imgur.com/DJtGHi4.png" alt="Buzz" />
+      </div>
     </div>
   );
 }
