b2b772fcfd44688041676f78f8aa1c61bb22db59
Dor <dorzairi@ymail.com>
2021-05-07 10:19:49 -0400

    Updating: style on page 3

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -4,8 +4,10 @@ import "./Timeline.css";
 function Timeline() {
   return (
     <div id="Niv">
-      <div id="First">First time line </div>
-      <div id="Second">time line </div>
+      <div id="First">First time line </div><hr id="firstLine" />
+        <h1 id="FirstTimeLine">2018 </h1>
+      <div id="Second">Second time line</div><h1 id="FirstTimeLine">2018</h1><hr id="secondLine" />
+
       <div id="Third">time line </div>
       <div id="Forth">time line </div>
       <div id="Fifth">time line </div>
