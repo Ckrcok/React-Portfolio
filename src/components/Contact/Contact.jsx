4a0c27ad7e0b0e865044f7b2b8d5577f15a99e4c
Dor <dorzairi@ymail.com>
2021-05-10 14:10:02 -0400

    added contact me section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,17 +5,31 @@ function Contact() {
   return (
     <div className="card" id="Contact">
       <h1 id="header" className="title ">
-        My Contact
+        Want To Talk?
       </h1>
-      ( // Bulma)
       <article className="message is-medium">
         <div className="message-header">
           <p id="message-header">Medium message</p>
         </div>
         <div className="message-body" id="message-body">
-          <div id="linkedin">This is my LinkedIn</div>
-          <div id="gitHub">This is my GitHub</div>
-          <div id="Email">This Is My Email</div>
+          <div id="linkedin">
+            <i class="fab fa-linkedin fa-2x">
+              {" "}
+              <a href="https://www.linkedin.com/in/dorz/">DorZairi</a>
+            </i>
+          </div>
+          <div id="gitHub">
+            <i class="fab fa-github-alt fa-2x">
+              {" "}
+              <a href="github.com/ckrcok">Ckrcok</a>
+            </i>
+          </div>
+          <div id="Email">
+            <i class="fas fa-inbox fa-2x">
+              {" "}
+              <a href="mailto:dorzairi@ymail.com"> Email </a>
+            </i>
+          </div>
         </div>
       </article>
     </div>
