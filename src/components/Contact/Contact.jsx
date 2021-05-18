633a9666432eda5ba2f13ff6942e64d3e3154388
Dor <dorzairi@ymail.com>
2021-05-18 13:24:30 -0400

    Changes some style and placement of contact me

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -3,31 +3,32 @@ import "./Contact.css";
 
 function Contact() {
   return (
-    <div className="card" id="Contact">
-      <h1 id="header" className="title ">
-        Want To Talk?
+    <div className="" id="Contact">
+      <h1 id="header" className="title" data-aos="fade-up">
+        Lets Connect!
       </h1>
-      <article className="message is-medium">
-        <div className="message-body" id="message-body">
+      <hr id="contactHr" data-aos="fade-up" />
+      <article className="">
+        <div className="" id="message-body">
           <img
             alt="Dor's Avatar"
             id="Avatar"
             src="https://avatars.githubusercontent.com/u/56084381?v=4"
           />
-          <div id="linkedin">
-            <i className="fab fa-linkedin fa-2x">
+          <div id="linkedin" data-aos="fade-up">
+            <i className="fab fa-linkedin fa-2x" id="linkedinIcon">
               {" "}
-              <a href="https://www.linkedin.com/in/dorz/">DorZairi</a>
+              <a href="https://www.linkedin.com/in/dorz/">Dor Zairi</a>
             </i>
           </div>
-          <div id="gitHub">
-            <i className="fab fa-github-alt fa-2x">
+          <div id="gitHub" data-aos="fade-up">
+            <i className="fab fa-github-alt fa-2x" id="gitHubIcon">
               {" "}
               <a href="https://www.github.com/ckrcok">Ckrcok</a>
             </i>
           </div>
-          <div id="Email">
-            <i className="fas fa-inbox fa-2x">
+          <div id="Email" data-aos="fade-up">
+            <i className="fas fa-inbox fa-2x" id="EmailIcon">
               {" "}
               <a href="mailto:dorzairi@ymail.com"> Email </a>
             </i>
